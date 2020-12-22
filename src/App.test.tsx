41c37a9ff59854eb40fab86cf86efedc5567cc1d
durgakiran <gatemefuture@gmail.com>
2020-12-22 21:49:57 +0530

    react with typescript setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,9 @@
-/* eslint-disable no-undef */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByTestId(/App/i);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
